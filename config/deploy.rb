75a2c3c3b031c56259955a2e3d4e2164b8e6d81b
anil-rathod11 <anil.rathod@anveshak.com>
2023-02-05 18:22:23 +0530

    adding gitignore

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,25 @@
+lock "~> 3.17.1"
 set :application, "jwt_authentication"
-set :repo_url, "git@github.com:anil-rathod11/jwt_authentication.git"
-# restart app by running: touch tmp/restart.txt
-# at server machine
-set :passenger_restart_with_touch, true
-set :rails_env, :development
-set :puma_threads, [4, 16]
-# Don’t change these unless you know what you’re doing
-set :pty, true
+set :repo_url, 'git@github.com:anil-rathod11/jwt_authentication.git'
+set :deploy_to, '/home/ubuntu/jwt_authentication'
 set :use_sudo, true
-set :stage, :production
-set :deploy_via, :remote_cache
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log, "#{release_path}/log/puma.access.log"
+set :branch, 'master'
+set :linked_files, %w{config/master.key config/database.yml}
+set :rails_env, 'production'
+set :keep_releases, 2
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_files, %w{config/database.yml config/master.key}
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_error_log,  "#{release_path}/log/puma.error.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, false 
-# set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
-set :deploy_to, "/var/www/jwt_authentication"
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
@@ -28,35 +28,5 @@ namespace :puma do
       execute "mkdir #{shared_path}/tmp/pids -p"
     end
   end
-
   before :start, :make_dirs
-end
-namespace :deploy do
-  desc "Make sure local git is in sync with remote."
-  task :check_revision do
-    on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/main`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
-        exit
-      end
-    end
-  end
-  desc "Initial Deploy"
-  task :initial do
-    on roles(:app) do
-      before "deploy:restart", "puma:start"
-      invoke "deploy"
-    end
-  end
-  desc "Restart application"
-    task :restart do
-      on roles(:app), in: :sequence, wait: 5 do
-      invoke "puma:restart"
-    end
-  end
-  before :starting, :check_revision
-  after :finishing, :compile_assets
-  after :finishing, :cleanup
-  after :finishing, :restart
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,6 @@
-set :stage, :production
-set :rails_env, :production
-set :branch, "main"
-server "18.117.152.92", user: "ubuntu", roles: %w{web app db}
+server '18.117.152.92', user: 'ubuntu', roles: %w{web app db}
+set :ssh_options, {
+forward_agent: true,
+auth_methods: %w[publickey],
+keys: %w[/home/anveshak/.ssh/employee_portal.pem]
+}
